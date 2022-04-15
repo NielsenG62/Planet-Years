f01779eb327bd2cfbdc3e02fc25049dfaf1d0ebf
Gabe Nielsen <gabenielsen7@gmail.com>
2022-04-15 14:02:17 -0700

    add code to index.js

--- src/index.js
+++ src/index.js
@@ -2,3 +2,13 @@ import $ from "jquery";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
+import { User } from "./js/user";
+
+$("form").submit(function (event) {
+  event.preventDefault();
+  const age = parseInt($("#age").val());
+  const planet = $("#planet-select").val();
+  const user = new User(age);
+  $("#planet-age").text(user.planetAge(planet, user.dayAge));
+  $("#life-expectancy").text(user.lifeExpectancy(planet));
+});
