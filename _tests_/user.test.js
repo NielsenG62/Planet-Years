2b2aa3517bb89c8dd5450c1bf2e6f0d7d294c523
Gabe Nielsen <gabenielsen7@gmail.com>
2022-04-15 14:55:16 -0700

    add test for if lifeExpectancy is 0

--- _tests_/user.test.js
+++ _tests_/user.test.js
@@ -13,15 +13,19 @@ describe("User", () => {
   test("should return the user's age in Earth years", () => {
     expect(user.planetAge("earth", user.dayAge)).toEqual(25);
   });
+
   test("should return the user's age in Mercury years", () => {
     expect(user.planetAge("mercury", user.dayAge)).toEqual(103);
   });
+
   test("should return the user's age in Venus years", () => {
     expect(user.planetAge("venus", user.dayAge)).toEqual(40);
   });
+
   test("should return the user's age in Mars years", () => {
     expect(user.planetAge("mars", user.dayAge)).toEqual(13);
   });
+
   test("should return the user's age in Jupiter years", () => {
     expect(user.planetAge("jupiter", user.dayAge)).toEqual(2);
   });
@@ -30,19 +34,29 @@ describe("User", () => {
   test("should calculate the life expectancy of the user in earth years", () => {
     expect(user.lifeExpectancy("earth")).toEqual(50);
   });
+
   test("should calculate the life expectancy of the user in Jupiter years", () => {
     expect(user.lifeExpectancy("jupiter")).toEqual(4);
   });
+
   test("should calculate how many years over the life expectancy is, if age is above it", () => {
     user = new User(90);
     expect(user.lifeExpectancy("earth")).toEqual(
       "You are 15 years over the average life expectancy on this planet."
     );
   });
+
   test("should calculate how many years over the life expectancy on Mercury is, if age is above it", () => {
     user = new User(90);
     expect(user.lifeExpectancy("mercury")).toEqual(
       "You are 63 years over the average life expectancy on this planet."
+    );
+  });
+
+  test("should give special message if lifeExpectancy is 0", () => {
+    user = new User(75);
+    expect(user.lifeExpectancy("earth")).toEqual(
+      "You are at the average life expectancy on this planet."
     );
   });
 });
