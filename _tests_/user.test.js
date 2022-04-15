6c9666572455973ec05568e8a1bf1b11148e91dd
Gabe Nielsen <gabenielsen7@gmail.com>
2022-04-15 08:30:07 -0700

    add more to the constructor test

--- _tests_/user.test.js
+++ _tests_/user.test.js
@@ -1,7 +1,8 @@
 import { User } from "./../src/js/user.js";
 describe("User", () => {
   test("should correctly create a user object with their age", () => {
-    const user = new User(25);
+    const user = new User("Gabe", 25);
+    expect(user.name).toEqual("Gabe");
     expect(user.age).toEqual(25);
   });
 });
