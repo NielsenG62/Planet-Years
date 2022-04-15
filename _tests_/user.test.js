32ac798f9506cc87afd08300be2d6334480a8131
Gabe Nielsen <gabenielsen7@gmail.com>
2022-04-15 08:37:45 -0700

    add BeforeEach to DRY test code

--- _tests_/user.test.js
+++ _tests_/user.test.js
@@ -1,7 +1,11 @@
 import { User } from "./../src/js/user.js";
 describe("User", () => {
+  let user;
+  beforeEach(() => {
+    user = new User("Gabe", 25);
+  });
+
   test("should correctly create a user object with their age", () => {
-    const user = new User("Gabe", 25);
     expect(user.name).toEqual("Gabe");
     expect(user.age).toEqual(25);
   });
